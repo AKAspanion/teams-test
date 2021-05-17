9610fb6218cc5ef1298831d24446091de78dc86c
Ankit Kumar Pandit <ankitp@geekyants.com>
2021-05-17 22:05:26 +0530

    error fixes

--- src/views/error/index.tsx
+++ src/views/error/index.tsx
@@ -1,11 +1,16 @@
+import React from 'react';
 import { FallbackProps } from 'react-error-boundary';
+
+import { Container } from '../../components';
 
 const ErrorFallback = ({ error }: FallbackProps) => {
   return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error.message}</pre>
-    </div>
+    <Container>
+      <div className="max-w-screen-lg mx-auto" role="alert">
+        <p className="text-3xl">Something went wrong</p>
+        <pre className="pb-4">{error.message}</pre>
+      </div>
+    </Container>
   );
 };
 
